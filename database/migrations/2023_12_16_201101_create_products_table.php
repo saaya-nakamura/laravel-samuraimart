785c47565ce4b09ca77fe4933576c55467deba65
saaya-nakamura <saaya.tsujino@gmail.com>
2023-12-27 17:26:12 +0900

    3回目のコミット

--- database/migrations/2023_12_16_201101_create_products_table.php
+++ database/migrations/2023_12_16_201101_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories','products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
